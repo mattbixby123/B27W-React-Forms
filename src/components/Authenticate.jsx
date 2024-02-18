65f9df6f16dca96aed762a4a78b03cb7cb506514
Matthew Bixby <matthewbixby@matthews-air.myfiosgateway.com>
2024-02-18 13:03:47 -0500

    authenticate component completed

--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -1,5 +1,38 @@
-function Authenticate() {
-  return <h2>Authenticate!</h2>;
+import React, { useState } from "react";
+
+function Authenticate({ token }) {
+  const [successMessage , setSuccessMessage] = useState(null);
+  const [error, setError] = useState(null);
+
+  async function handleClick() {
+    try {
+      // Send a GET request to the /authenticate endpoint with the token in the Authentication header
+      const response = await fetch(
+        "https://fsa-jwt-practice.herokuapp.com/authenticate",
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`, // format we use to pass token in the Authorization header
+        },
+      }
+      );
+      const result = await response.json();
+      // console.log(result); // shows the results of the Token Authentication
+      setSuccessMessage(result.message);
+
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
+  return (
+    <div>
+      <h2>Authenticate Component</h2>
+      {successMessage && <p>{successMessage}</p>}
+      {error && <p>{error}</p>}
+      <button onClick={handleClick}>Authenticate Token!</button>
+    </div>
+  );
 }
 
 export default Authenticate;
